e16ccad12c4152633125ec6c6182fa945fddde20
Ankit Kumar Yadav <ankitkryadavchapoli@gmail.com>
2026-04-19 12:01:00 +0530

    added sandbox args in puppeteer

--- backend/src/services/generatePDF.service.js
+++ backend/src/services/generatePDF.service.js
@@ -1,7 +1,7 @@
 import puppeteer from "puppeteer";
 
 export const generatePdfFromHtml = async (htmlContent) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
 
   await page.setContent(htmlContent, { waitUntil: "networkidle0" });
