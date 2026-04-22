f8824f80dcbcd361a4f3c26dfe60ab23d13119f0
Ankit Kumar Yadav <ankitkryadavchapoli@gmail.com>
2026-04-22 22:19:21 +0530

    update CORS origin in app.js and set baseURL to environment variable in interview.api.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"https://ai-5musa78ou-ankryv1s-projects.vercel.app",
+    origin:"https://ai-job-sable.vercel.app",
     credentials: true
 }))
 
