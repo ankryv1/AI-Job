72aaaba9000c827c7f7867b4f6dc7d219689a99b
Ankit Kumar Yadav <ankitkryadavchapoli@gmail.com>
2026-04-24 10:59:32 +0530

    added stability flag for puppeteer yo run smoothly in production

--- backend/src/services/generatePDF.service.js
+++ backend/src/services/generatePDF.service.js
@@ -1,7 +1,15 @@
 import puppeteer from "puppeteer";
 
 export const generatePdfFromHtml = async (htmlContent) => {
-  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage"
+    ]
+  });
+
   const page = await browser.newPage();
 
   await page.setContent(htmlContent, { waitUntil: "networkidle0" });
